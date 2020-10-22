97477f4608a319bde935529c663247e2dccc98f1
Anna Fetsun <popcorn@Annas-MacBook-Pro.local>
2020-10-23 01:03:57 +0300

    add html page

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # fastapi_docker
+
+docker-compose up -d --build
+
+url: 
+http://localhost:8080
+
+http://localhost:8080/api/v1/patients
+http://localhost:8080/api/v1/patients/docs
+
+http://localhost:8080/api/v1/drugs
+http://localhost:8080/api/v1/drugs/docs
--- compose_fastapi/patient-service/app/api/service.py
+++ compose_fastapi/patient-service/app/api/service.py
@@ -2,7 +2,7 @@
 import os
 import httpx
 
-DRUGS_SERVICE_HOST_URL = 'http://192.168.16.6:8080/api/v1/drugs/'
+DRUGS_SERVICE_HOST_URL = 'http://localhost:8080/api/v1/drugs/'
 
 def is_drug_present(drug_id: int):
     url = os.environ.get('DRUG_SERVICE_HOST_URL') or DRUGS_SERVICE_HOST_URL
